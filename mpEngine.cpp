010f21bc7fc8089d50628a3a10323b70f1e27d37
fredricksonP <112718203+fredricksonP@users.noreply.github.com>
2023-10-13 11:48:28 -0600

    FP Cam now moves w/ arrow keys

--- mpEngine.cpp
+++ mpEngine.cpp
@@ -669,14 +669,14 @@ void mpEngine::_updateScene() {
     //Code to rotate the look direction of the FP cam
     if (_currentCam == 1) {
         // Update yaw based on 'A' and 'D' keys
-        if (_keys[GLFW_KEY_A] && !_keys[GLFW_KEY_D]) {
+        if ( (_keys[GLFW_KEY_A] || _keys[GLFW_KEY_LEFT]) && (!_keys[GLFW_KEY_D] || !_keys[GLFW_KEY_RIGHT])) {
             GLfloat currAngle = _currentPlayer->getAngle();
             currAngle += _cameraSpeed.x;
             // Turn left by decreasing yaw
             //TODO: fix the hard encoding of the FP yaw adjustment
             fpCam->setYaw(fpCam->getYaw() - 1.13);
             //fpCam->setYaw(currAngle);
-        } else if (_keys[GLFW_KEY_D] && !_keys[GLFW_KEY_A]) {
+        } else if ((_keys[GLFW_KEY_D] || _keys[GLFW_KEY_RIGHT]) && (!_keys[GLFW_KEY_A] || !_keys[GLFW_KEY_LEFT])) {
             // Turn right by increasing yaw
             //TODO: fix the hard encoding of the FP yaw adjustment
             fpCam->setYaw(fpCam->getYaw() + 1.13);
